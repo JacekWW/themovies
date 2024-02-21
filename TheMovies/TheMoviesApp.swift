606cc9120e2654364e0bde3ac18dd6a75e89ad38
Jacek Wierzbicki-Wos <jwierzbickiwos@gmail.com>
2024-02-21 14:01:06 +0100

    initial project

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Jacek Wierzbicki-Wos on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
